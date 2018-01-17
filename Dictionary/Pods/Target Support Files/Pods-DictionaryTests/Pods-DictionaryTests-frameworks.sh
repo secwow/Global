adec6215f7a39cc9e9fcd8d8ba782ad5c9e0e715
Dmytro Rebenko <dima.rebenko@gmail.com>
2018-01-17 14:39:15 +0200

    Pod install

--- Dictionary/Pods/Target Support Files/Pods-DictionaryTests/Pods-DictionaryTests-frameworks.sh
+++ Dictionary/Pods/Target Support Files/Pods-DictionaryTests/Pods-DictionaryTests-frameworks.sh
@@ -100,15 +100,6 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/OCMock/OCMock.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/OCMock/OCMock.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ReactiveObjC/ReactiveObjC.framework"
-fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
